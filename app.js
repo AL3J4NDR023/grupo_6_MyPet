f029ff07646a8624a1718f523a56434c0618378f
unknown <luisfjimenezf@gmail.com>
2021-12-15 16:56:04 -0500

    Subiendo header y footer

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const path = require('path'); // funcion
 const publicPath = path.join(__dirname, 'public')// resolver ruta
 app.use(express.static(publicPath)); //traer los elementos public - recursos estaticos
 
-app.listen(3000,()=>{  //correr servidor
+app.listen(3001,()=>{  //correr servidor
     console.log("Server Run")
 })
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/home.html'));
+    res.sendFile(path.join(__dirname,'/views/index.html'));
 })
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname,'/views/carrito.html')));
