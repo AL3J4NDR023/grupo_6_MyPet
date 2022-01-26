e0e1a5c2fbaf85e72a6a98298c2f0fb5b66cdd79
luiscortesj1 <luiscortesj1999@gmail.com>
2022-01-25 21:24:07 -0500

    register-login rutas para home, con archivos partials.

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 /*********** REQUIRE CONTROLLERS ********************** */
 const controller = require('../controllers/homeController')
 router.get('/', controller.home);
+router.get('/login', controller.login);
+router.get('/register', controller.register);
 
 module.exports = router
